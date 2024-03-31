1fcbacf3e3a99b29858cd5f5ea1ed4ba7b386846
antonkarzov92 <100116785+antonkarzov92@users.noreply.github.com>
2024-03-31 19:35:43 +0300

    Update Predator.java

--- src/main/java/com/example/Predator.java
+++ src/main/java/com/example/Predator.java
@@ -1,12 +1,1 @@
-package com.example;
 
-import java.util.List;
-
-public interface Predator {
-
-    List<String> eatMeat() throws Exception;
-
-    int getKittens();
-
-    List<String> getFood(String str)throws Exception ;
-}
