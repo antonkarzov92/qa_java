5e70fc90dc00920727fe3a320c1dc454dd268261
antonkarzov92 <karzov.1992@mail.ru>
2024-07-20 21:10:30 +0300

    Финальные изменения

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -7,15 +7,23 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.Assert.assertArrayEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest extends TestCase {
     private static final String MALE = "Самец";
+    private static final String FEMALE = "Самка";
+    private static final String PREDATOR = "Хищник";
+    private static final String MEAT = "Мясо";
     private static final String UNSUPPORTED_SEX = "unsupported sex";
     private static final String TEXT_EXCEPTION = "Используйте допустимые значения пола животного - самей или самка";
     private Lion lion;
+
     @Mock
     private Feline feline;
 
@@ -38,10 +46,32 @@ public class LionTest extends TestCase {
     }
 
     @Test
-    public void testGetFood() throws Exception {
+    public void testDoesHaveMane() throws Exception {
         lion = new Lion(MALE, feline);
 
-        lion.getFood();
+        boolean actual = lion.doesHaveMane();
+        assertEquals("Должен иметь гриву",
+                true, actual);
+    }
+
+    @Test
+    public void testDoesNotHaveMane() throws Exception {
+        lion = new Lion(FEMALE, feline);
+
+        boolean actual = lion.doesHaveMane();
+        assertEquals("Не должен иметь гриву",
+                false, actual);
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        var expected = List.of(MEAT);
+        Mockito.when(feline.getFood(PREDATOR)).thenReturn(expected);
+
+        lion = new Lion(MALE, feline);
+
+        var actual = lion.getFood();
         Mockito.verify(feline).getFood(Mockito.anyString());
+        assertArrayEquals("Возвращено некорректное значение для еды", actual.toArray(), expected.toArray());
     }
 }
